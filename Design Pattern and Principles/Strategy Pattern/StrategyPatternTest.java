6972410dea8bc8ab8ab3e1b7f38e3c5db91b4516
Ajitha <ajithaselvan001@gmail.com>
2026-06-22 11:49:20 +0530

    Add files via upload

--- Design Pattern and Principles/Strategy Pattern/StrategyPatternTest.java
+++ Design Pattern and Principles/Strategy Pattern/StrategyPatternTest.java
@@ -1,1 +1,44 @@
+public class StrategyPatternTest {
 
+    interface PaymentStrategy {
+        void pay(double amount);
+    }
+
+    static class CreditCardPayment implements PaymentStrategy {
+
+        public void pay(double amount) {
+            System.out.println("Paid Rs." + amount + " using Credit Card");
+        }
+    }
+
+    static class PayPalPayment implements PaymentStrategy {
+
+        public void pay(double amount) {
+            System.out.println("Paid Rs." + amount + " using PayPal");
+        }
+    }
+
+    static class PaymentContext {
+
+        private PaymentStrategy strategy;
+
+        public void setPaymentStrategy(PaymentStrategy strategy) {
+            this.strategy = strategy;
+        }
+
+        public void executePayment(double amount) {
+            strategy.pay(amount);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        PaymentContext context = new PaymentContext();
+
+        context.setPaymentStrategy(new CreditCardPayment());
+        context.executePayment(5000);
+
+        context.setPaymentStrategy(new PayPalPayment());
+        context.executePayment(2500);
+    }
+}
